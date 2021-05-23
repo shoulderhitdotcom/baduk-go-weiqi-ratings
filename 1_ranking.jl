c9e7f87f823896ccddba2a836bfd057161ae27af
xiaodaigh <zhuojia.dai@gmail.com>
2021-05-23 14:57:40 +1000

    ;updated

--- 1_ranking.jl
+++ 1_ranking.jl
@@ -23,6 +23,7 @@ if !isdir("kifu-depot-games-for-ranking.jdf/")
         750 => 7.5,
         605.0 => 6.5
     )
+
     tbl = @where(tbl, in.(:komi_fixed, Ref((6.5, 7.5))))
     select!(tbl, Not([:sgf, :comp, :result, :kifu_link, :win_by, :komi]))
 
@@ -36,14 +37,33 @@ tbl = JDF.load("kifu-depot-games-for-ranking.jdf/") |> DataFrame
 mad = maximum(tbl.date)
 @time pings, games, white75_advantage, black65_advantage= estimate_rating(mad-Day(364), mad; tbl);
 
+from_date, to_date = string.(extrema(games.date))
+
+JDF.save("pings.jdf/", pings)
+
+
+replacements = (
+    "{{ngames}}" => string(nrow(games)),
+    "{{from_date}}" =>from_date,
+    "{{to_date}}" => to_date,
+    "{{elo_white75_adv}}" => string(round(Int, white75_advantage*400/log(10))),
+    "{{elo_black65_adv}}" => string(round(Int, black65_advantage*400/log(10))),
+    "{{ping_white75_adv}}" => string(round(white75_advantage, digits=2)),
+    "{{ping_black65_adv}}" => string(round(black65_advantage, digits=2)),
+)
+
 using Weave
 
 weave("index.jmd", out_path = "index-tmp.md", doctype = "github")
+
 
 open("index-tmp.md") do file
     outfile = open("index.md", "w")
     while !eof(file)
         line = readline(file)
+        for (in, replace_with) in replacements
+            line = replace(line, in=>replace_with)
+        end
         if line != "```"
             write(outfile, line)
             write(outfile, "\n")
@@ -53,3 +73,32 @@ open("index-tmp.md") do file
 end
 
 rm("index-tmp.md")
+
+;`git status`
+
+## make a GLM solution
+## Doesn't work
+
+# players = vcat(games.black, games.white) |> unique |> sort!
+
+# m = zeros(nrow(games), length(players))
+
+# for (row, i) in enumerate(indexin(games.black, players))
+#     m[row, i] += 1
+# end
+
+# for (row, i) in enumerate(indexin(games.white, players))
+#     m[row, i] -= 1
+# end
+
+# df = DataFrame(m, :auto)
+
+# df.y = float.(games.who_win .== "B")
+
+# using GLM: Term, glm, Binomial, LogitLink
+
+# form = mapreduce(Term, + , [Symbol("x"*string(i)) for i in 1:length(players)])
+
+# @time glm(Term(:y)~form, df, Binomial(), LogitLink());
+
+
